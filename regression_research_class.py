4271ba02bd768d65a4af048e7b302cd3ce4318b0
denis-42ds <zaytseff.denis@gmail.com>
2024-05-16 22:22:18 +0300

    research_class modified, baseline cv added

--- regression_research_class.py
+++ regression_research_class.py
@@ -1,10 +1,18 @@
 import os
 import random
-# import mlflow
+import mlflow
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import psycopg2 as psycopg
 import matplotlib.pyplot as plt
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from category_encoders import CatBoostEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, root_mean_squared_error, mean_absolute_percentage_error
 
 RANDOM_STATE = 42
 random.seed(RANDOM_STATE)
@@ -44,7 +52,7 @@ class DatasetExplorer:
                 sns.heatmap(dataset.isnull(), cmap=sns.color_palette(['#000099', '#ffff00']))
                 plt.xticks(rotation=90)
                 plt.title('Visualization of the number of missing values', size=12, y=1.02)
-                if assets_dir:
+                if assets_dir is not None:
                     plt.savefig(os.path.join(assets_dir, 'Visualization of the number of missing values.png'))
                 plt.show()
 
@@ -77,11 +85,79 @@ class DatasetExplorer:
 
         return dataset
 
+    def data_splitting(self, dataset=None, pred_period=None, target=None):
+        dataset.sort_index(inplace=True)
+        y = dataset[target]
+        X = dataset.drop([target], axis=1)
+
+        X_train = X[X.index <= (X.index.max() - pd.DateOffset(days=pred_period))]
+        X_test = X[X.index > (X.index.max() - pd.DateOffset(days=pred_period))]
+        y_train = y[:X_train.shape[0]]
+        y_test = y[X_train.shape[0]:]
+
+        print('Размерности полученных выборок:')
+        display(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+
+        return X_train, y_train, X_test, y_test
+
     def data_preprocessing(self):
         pass
 
-    def model_fitting(self, model_name=None, features=None, labels=None, params=None):
-        pass
+    def model_fitting(self, model_name=None, train_features=None, train_labels=None, tscv=None, params=None, params_selection=False):
+
+        # функция для расчёта метрики
+        def wape(y_true: np.array, y_pred: np.array):
+            return np.sum(np.abs(y_true-y_pred))/np.sum(np.abs(y_true))
+
+        if model_name == 'Baseline':
+            model = LinearRegression()
+
+        binary_features = train_features.loc[:, train_features.nunique() == 2].columns.to_list()
+        cat_features = ['st_id', 'pr_group_id']
+
+        preprocessor = ColumnTransformer(
+            [
+                ('binary', OneHotEncoder(drop='if_binary'), binary_features),
+                ('cat', CatBoostEncoder(), cat_features)
+            ],
+            remainder='drop',
+            verbose_feature_names_out=False
+        )
+
+        pipeline = Pipeline(
+            [
+                ('preprocessor', preprocessor),
+                ('model', model)
+            ]
+        )
+
+        metrics = {
+            'wape': [],
+            'mape': [],
+            'rmse': [],
+            'mae': [],
+            'mse': [],
+            'r2': []
+        }
+        for train_index, test_index in tscv.split(train_features):
+            X_train_fold, X_test_fold = train_features.iloc[train_index], train_features.iloc[test_index]
+            y_train_fold, y_test_fold = train_labels.iloc[train_index], train_labels.iloc[test_index]
+    
+            pipeline.fit(X_train_fold.drop('pr_sku_id', axis=1), y_train_fold)
+            y_pred = pipeline.predict(X_test_fold.drop('pr_sku_id', axis=1))
+    
+            metrics['wape'].append(round(wape(y_test_fold, y_pred), 3))
+            metrics['mape'].append(round(mean_absolute_percentage_error(y_test_fold, y_pred), 3))
+            metrics['rmse'].append(round(root_mean_squared_error(y_test_fold, y_pred), 3))
+            metrics['mae'].append(round(mean_absolute_error(y_test_fold, y_pred), 3))
+            metrics['mse'].append(round(mean_squared_error(y_test_fold, y_pred), 3))
+            metrics['r2'].append(round(r2_score(y_test_fold, y_pred), 3))
+
+        valid_metrics = {metric: round(np.mean(values), 3) for metric, values in metrics.items()}
+        print('Средние значения метрик по кросс-валидации:')
+        display(valid_metrics)
+
+        return valid_metrics, pipeline
 
     def model_logging(self,
 					  experiment_name=None,
@@ -91,8 +167,8 @@ class DatasetExplorer:
 					  metrics=None,
 					  model=None,
 					  train_data=None,
-					  test_data=None,
-					  test_label=None,
+                      train_label=None,
+					  assets_dir=None,
 					  metadata=None,
 					  code_paths=None,
 					  tsh=None,
@@ -103,26 +179,40 @@ class DatasetExplorer:
         experiment = mlflow.get_experiment_by_name(experiment_name)
         experiment_id = mlflow.set_experiment(experiment_name).experiment_id
 		
-        if run_name == 'baseline_1_all_data':
-            with mlflow.start_run(run_name=run_name, experiment_id=experiment_id) as run:
-                run_id = run.info.run_id
-                mlflow.log_metrics(metrics)
-        else:
-            pip_requirements = "requirements.txt"
-            try:
-                signature = mlflow.models.infer_signature(test_data, test_label.values)
-            except:
-                signature = mlflow.models.infer_signature(test_data, test_label)
-            input_example = (pd.DataFrame(train_data)).iloc[0].to_dict()
+        pip_requirements = 'requirements.txt'
+        try:
+            signature = mlflow.models.infer_signature(train_data.values, train_label.values)
+        except:
+            signature = mlflow.models.infer_signature(train_data, train_label)
+        # input_example = (pd.DataFrame(train_data)).iloc[0].to_dict()
+        input_example = train_data[:10]
 
+        if 'catboost' in registry_model:
             with mlflow.start_run(run_name=run_name, experiment_id=experiment_id) as run:
                 run_id = run.info.run_id
-                mlflow.log_artifacts(self.ASSETS_DIR)
+                mlflow.log_artifacts(assets_dir)
+                mlflow.log_params(params)
+                mlflow.log_metrics(metrics)
+                model_info = mlflow.catboost.log_model(
+                    cb_model=model,
+                    artifact_path='models',
+                    pip_requirements=pip_requirements,
+                    signature=signature,
+                    input_example=input_example,
+                    metadata=metadata,
+                    code_paths=code_paths,
+                    registered_model_name=registry_model,
+                    await_registration_for=60
+				)
+        else:
+            with mlflow.start_run(run_name=run_name, experiment_id=experiment_id) as run:
+                run_id = run.info.run_id
+                mlflow.log_artifacts(assets_dir)
                 mlflow.log_params(params)
                 mlflow.log_metrics(metrics)
                 model_info = mlflow.sklearn.log_model(
                     sk_model=model,
-                    artifact_path="models",
+                    artifact_path='models',
                     pip_requirements=pip_requirements,
                     signature=signature,
                     input_example=input_example,
@@ -132,8 +222,58 @@ class DatasetExplorer:
                     await_registration_for=60
 				)
 
-    def model_analysis(self, model):
-        pass
+    def models_comparison(self, connection=None, postgres_credentials=None, experiment_name=None, assets_dir=None):
+        connection.update(postgres_credentials)
+        with psycopg.connect(**connection) as conn:
+            with conn.cursor() as cur:
+                cur.execute('''
+                            SELECT
+                              experiments.name AS experiment_name,
+                              runs.name AS run_name,
+                              model_versions.name AS model_name,
+                              model_versions.version AS model_version,
+                              MAX(CASE WHEN metrics.key = 'wape' THEN metrics.value END) AS wape,
+                              MAX(CASE WHEN metrics.key = 'mape' THEN metrics.value END) AS mape,
+                              MAX(CASE WHEN metrics.key = 'rmse' THEN metrics.value END) AS rmse,
+                              MAX(CASE WHEN metrics.key = 'mae' THEN metrics.value END) AS mae,
+                              MAX(CASE WHEN metrics.key = 'mse' THEN metrics.value END) AS mse,
+                              MAX(CASE WHEN metrics.key = 'r2' THEN metrics.value END) AS r2
+                            FROM experiments
+                              LEFT JOIN runs USING (experiment_id)
+                              LEFT JOIN metrics USING (run_uuid)
+                              LEFT JOIN model_versions ON model_versions.run_id=runs.run_uuid
+                            WHERE
+                              experiments.name = %s
+                            GROUP BY
+                              experiments.name,
+                              runs.name,
+                              model_versions.name,
+                              model_versions.version
+                            ORDER BY wape
+                            ''', (experiment_name,))
+                table_data = cur.fetchall()
+                table_columns = [desc[0] for desc in cur.description]
+                print('Models and their metrics:')
+                models_data = pd.DataFrame(table_data, columns=table_columns)
+                display(models_data)
+
+        plt.figure(figsize=(14, 8))
+        sns.set_palette("husl")
+        metrics = ['wape', 'mape', 'r2', 'mse', 'rmse', 'mae']
+
+        for i, metric in enumerate(metrics):
+            if i < 3:
+                plt.subplot(2, 3, i+1)
+            else:
+                plt.subplot(2, 3, i+1)
+            sns.barplot(x='model_name', y=metric, data=models_data)
+            plt.title(f'Comparison of {metric.upper()}')
+            plt.xticks(rotation=45, ha='right')
+
+        plt.tight_layout()
+        if assets_dir is not None:
+            plt.savefig(os.path.join(assets_dir, 'Comparisons of models metrics.png'))
+        plt.show()
 
     def genre_rec_sys(self, dataset=None, image_name=None, emb_array=None, num_recommendations=3):
         pass
